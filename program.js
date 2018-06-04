e7d145bff5eaa03a6a508532e2ffe7589aed20cd
CiesielskiK <39362820+CiesielskiK@users.noreply.github.com>
2018-06-04 11:01:21 +0200

    Add files via upload

--- program.js
+++ program.js
@@ -1,18 +1,29 @@
+var OSinfo = require('./OSinfo');
+
+
 process.stdin.setEncoding('utf-8');
 process.stdin.on('readable', function(){
-        var input = process.stdin.read();
-        if (input!==null){
-        var instruction = input.toString().trim();
-        switch(instruction){
-                case 'exit':
-                        process.stdout.write('Quitting app!)\n');
-                        console.log('Wersja node.js: ' + process.version);
-                        process.exit();
-                        break;
-                default:
-                        process.stderr.write('bledna instrukcja!\n');
-                        };
-                };
+	var input = process.stdin.read();
+	if (input!==null){
+	var instruction = input.toString().trim();
+	switch(instruction){
+		case 'exit':
+			process.stdout.write('Quitting app!)\n');
+			console.log('Wersja node.js: ' + process.version);
+			process.exit();
+			break;
+		case '/sayhello':
+			process.stdout.write('hello!\n');
+			break;
+		case '/getOSinfo':
+			OSinfo.print();
+			break;
+	default:
+			process.stderr.write('bledna instrukcja!\n');
+			};
+		};
 });
+
+
 
 
